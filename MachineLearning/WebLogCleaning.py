3b428774d3c29022a72a6127e9181163558c748e
Divyalok Jaiswal <divyjais2001@gmail.com>
2021-07-05 18:31:39 +0530

    completed web log cleaning tut and added assets to gitignore

--- MachineLearning/WebLogCleaning.py
+++ MachineLearning/WebLogCleaning.py
@@ -1,2 +1,48 @@
 #An example of data cleaning which should be done before analysing data
 
+import re
+
+filepath = '../assets/access_log.txt'
+
+format_pat= re.compile(
+    r"(?P<host>[\d\.]+)\s"
+    r"(?P<identity>\S*)\s"
+    r"(?P<user>\S*)\s"
+    r"\[(?P<time>.*?)\]\s"
+    r'"(?P<request>.*?)"\s'
+    r"(?P<status>\d+)\s"
+    r"(?P<bytes>\S*)\s"
+    r'"(?P<referer>.*?)"\s'
+    r'"(?P<user_agent>.*?)"\s*'
+)
+
+URLCounts = {}
+
+# i = 0;
+with open(filepath, "r") as f:
+    for line in (l.rstrip() for l in f):
+        match= format_pat.match(line)
+        if match:
+            access = match.groupdict()
+            agent = access['user_agent']
+            if (not('bot' in agent or 'spider' in agent or 
+                    'Bot' in agent or 'Spider' in agent or
+                    'W3 Total Cache' in agent or agent =='-')):
+                request = access['request']
+                fields = request.split()    
+                # if i == 0:
+                #     print(fields)
+                #     i+=1
+                if (len(fields) == 3):
+                    (action, URL, protocol) = fields
+                    if (URL.endswith("/") and 'feed' not in URL):
+                        if (action == 'GET'):
+                            if URL in URLCounts:
+                                URLCounts[URL] = URLCounts[URL] + 1
+                            else:
+                                URLCounts[URL] = 1
+
+results = sorted(URLCounts, key=lambda i: int(URLCounts[i]), reverse=True)
+
+for result in results[:20]:
+    print(result + ": " + str(URLCounts[result]))
